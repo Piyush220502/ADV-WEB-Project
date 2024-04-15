ffb79ed6c0f872d8152ca93394f14e1c3bf60831
Piyush220502 <167056296+Piyush220502@users.noreply.github.com>
2024-04-15 18:47:10 +0530

    Add files via upload

---
+++ CA3 Project Ad/FRONTEND/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">SKR</div>
+          <div className="right">
+            <p>Lovely Professional Universty, Punjab, India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By PIYUSH</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Piyush.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
